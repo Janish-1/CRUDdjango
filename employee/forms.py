b838768eef221c394b43fd9f178ae64bb644ba84
Janish-1 <janish.pancholi11@gmail.com>
2024-02-15 02:41:41 +0530

    Commit

--- employee/forms.py
+++ employee/forms.py
@@ -3,4 +3,5 @@ from employee.models import Employee
 class EmployeeForm(forms.ModelForm):  
     class Meta:  
         model = Employee  
-        fields = "__all__"  
+        fields = "__all__"
+          
